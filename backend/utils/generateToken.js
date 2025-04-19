267f7d572adda874ec3907eb53d03f4c48be0452
aditya <176797545+sk-adi@users.noreply.github.com>
2025-04-19 14:30:22 +0530

    update cookies setting for production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken"
 
 
 const generateToken=(res,userId)=>{
-    const token=jwt.sign({userId},process.env.SECRET_KEY,{expiresIn:"1h"})
+    const token=jwt.sign({userId},process.env.SECRET_KEY,{expiresIn:"24h"})
     res.cookie(
         'jwt',token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite:"lax",
-            maxAge:60*60*1000,
+            secure:true,
+            sameSite:"none",
+            maxAge:7*24*60*60*1000,
             path:'/',
         }
     )
